f6a7816c717230b01677d69caa57f1a1552498c8
Arti <145042815+Teemu-Wiskari@users.noreply.github.com>
2024-03-01 21:23:42 +0300

    Update_select
    Query improvements

--- select.sql
+++ select.sql
@@ -21,8 +21,9 @@ where name not like '% %';
 -- Название треков, которые содержат слово «my»
 
 select name from track
-where name ilike '%my %'
+where name ilike 'my %'
 or name ilike '% my'
+or name ilike '% my %'
 or name ilike 'my';
 
 -- Количество исполнителей в каждом жанре
@@ -47,10 +48,10 @@ group by a.name;
 -- Все исполнители, которые не выпустили альбомы в 2020 году
 
 select m.name from musician m 
-left join album_musician am on am.id_musician = m.id_musician 
-left join album a on a.id_album = am.id_album 
-where release_year not in (
-select release_year from album
+where m.name not in(
+select m.name from musician m
+left join album_musician am on am.id_musician = m.id_musician
+left join album a on a.id_album = am.id_album
 where release_year = '2020');
 
 -- Названия сборников, в которых присутствует конкретный исполнитель
@@ -65,13 +66,14 @@ where m.name = 'Oasis';
 
 -- Названия альбомов, в которых присутствуют исполнители более чем одного жанра
 
-select a.name from album a 
+select distinct a.name from album a 
 left join album_musician am on am.id_album = a.id_album 
 left join musician m on m.id_musician = am.id_musician 
 left join musician_genres mg on mg.id_musician = m.id_musician 
 left join music_genre g on g.id_genre = mg.id_genre 
-group by a.name
-having count (g.name) > 1;
+group by m.name, a.name
+having count (mg.id_genre) > 1;
+
 
 -- Наименование треков, которые не входят в сборники
 
@@ -90,16 +92,13 @@ having duration = (select min (duration) from track);
 
 -- Названия альбомов, содержащих наименьшее количество треков
 
-select a.name from album a 
-left join track t on t.id_album = a.id_album 
-where t.id_album in (
-select id_album from track
-group by id_album
-having count (id_album) = (
-select count (id_album) from track
+select a.name
+from album a left join track t on t.id_album = a.id_album 
+group by a.name
+having count (t.id_track) = (
+select count (t.id_track) from track t
 group by id_album 
-order by count (id_album)
-limit 1))
-order by a.name;
+order by 1
+limit 1);
 
 
